42544b0a89ba141e23aa16ee92c23507880317a4
Fer <fernandoj92@gmail.com>
2016-09-12 14:42:47 +0000

    Tour of Heroes finished

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,40 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 
-import { AppComponent } from './app.component'
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { AppComponent }         from './app.component';
+import { routing }              from './app.routing';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { HeroesComponent }      from './hero/heroes/heroes.component';
+import { HeroDetailComponent }  from './hero/hero-detail/hero-detail.component';
+import { HeroSearchComponent }  from './hero/hero-search/hero-search.component';
+import { HeroService }          from './hero/hero.service';
+
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [AppComponent],
-  bootstrap: [AppComponent]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    routing
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    HeroSearchComponent
+  ],
+  providers: [
+    HeroService,
+  ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
